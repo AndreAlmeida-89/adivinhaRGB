0f7e0ad5bb3e8f29ed047585b78cb9e36e7c3435
André Almeida <andre_rdo@hotmail.com>
2020-12-07 21:58:07 -0300

    Initial Commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Andre on 07/12/20.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
